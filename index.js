20024d612cdbee031f872b3e961910666281e81a
shefat26 <shefatmoral@gmail.com>
2024-12-17 22:59:16 -0500

    array prep

--- index.js
+++ index.js
@@ -1,1 +1,93 @@
 // Write your solution here!
+
+
+const cats = ["Milo","Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+
+    
+   cats.push(name)
+
+}
+destructivelyAppendCat("Ralph")
+
+//
+
+function destructivelyPrependCat(name){
+
+    
+   cats.unshift(name)
+
+}
+destructivelyPrependCat()
+
+
+
+//
+
+function destructivelyRemoveLastCat(name){
+
+
+    cats.pop()
+
+}
+destructivelyRemoveLastCat()
+
+
+//
+
+function destructivelyRemoveFirstCat(name){
+
+
+    cats.shift()
+
+}
+destructivelyRemoveFirstCat()
+
+
+
+//
+
+function appendCat(name){
+
+
+    return [...cats, name];
+
+}
+appendCat()
+
+
+
+//
+
+function prependCat(name){
+
+
+    return [name, ...cats];
+
+}
+prependCat()
+
+
+//
+
+function  removeLastCat(name){
+
+
+   return cats.slice(0,-1)
+
+}
+removeLastCat()
+
+
+function  removeFirstCat(name){
+
+
+    return cats.slice(1)
+
+}
+removeFirstCat()
+
+
+
+
